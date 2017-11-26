7bc52fd291bbf8c8432d84ce6ab14a134a5761ff
Guillermo González de Agüero <z06.guillermo@gmail.com>
2017-11-26 16:41:33 +0100

    Added JMod

--- impl/src/main/java/es/guillermogonzalezdeaguero/container/impl/Launcher.java
+++ impl/src/main/java/es/guillermogonzalezdeaguero/container/impl/Launcher.java
@@ -10,6 +10,11 @@ import java.io.IOException;
 public class Launcher {
 
     public static void main(String[] args) throws IOException {
+        /*Properties properties = new Properties();
+        properties.load(Launcher.class.getResourceAsStream("conf/app.properties"));
+
+        int port = Integer.valueOf(String.valueOf(properties.getOrDefault("port", 8383)));*/
+
         new Server(8282).start();
     }
 }
