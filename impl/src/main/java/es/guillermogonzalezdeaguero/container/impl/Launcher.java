2d950cc43efeb4dc617745e4d978d293d9dcd7e9
Guillermo González de Agüero <z06.guillermo@gmail.com>
2017-12-05 19:02:34 +0100

    Debug and basic custom config support

--- impl/src/main/java/es/guillermogonzalezdeaguero/container/impl/Launcher.java
+++ impl/src/main/java/es/guillermogonzalezdeaguero/container/impl/Launcher.java
@@ -1,7 +1,12 @@
 package es.guillermogonzalezdeaguero.container.impl;
 
 import es.guillermogonzalezdeaguero.container.impl.server.Server;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 /**
  *
@@ -10,11 +15,14 @@ import java.io.IOException;
 public class Launcher {
 
     public static void main(String[] args) throws IOException {
-        /*Properties properties = new Properties();
-        properties.load(Launcher.class.getResourceAsStream("conf/app.properties"));
+        Path baseDir = Paths.get(System.getProperty("server.basedir")).toAbsolutePath();
 
-        int port = Integer.valueOf(String.valueOf(properties.getOrDefault("port", 8383)));*/
+        Properties properties = new Properties();
+        try (InputStream is = new FileInputStream(baseDir.resolve("conf").resolve("app.properties").toFile())) {
+            properties.load(is);
+        }
+        int port = Integer.valueOf(String.valueOf(properties.getOrDefault("port", 8383)));
 
-        new Server(8282).start();
+        new Server(port).start();
     }
 }
