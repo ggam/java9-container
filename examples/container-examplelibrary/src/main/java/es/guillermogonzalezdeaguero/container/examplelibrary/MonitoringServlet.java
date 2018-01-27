22f46a55272aff1707c8264b0854c24e9fb4988a
Guillermo González de Agüero <z06.guillermo@gmail.com>
2018-01-27 14:02:23 +0100

    Improved example

--- examples/container-examplelibrary/src/main/java/es/guillermogonzalezdeaguero/container/examplelibrary/MonitoringServlet.java
+++ examples/container-examplelibrary/src/main/java/es/guillermogonzalezdeaguero/container/examplelibrary/MonitoringServlet.java
@@ -1,6 +1,8 @@
 package es.guillermogonzalezdeaguero.container.examplelibrary;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,13 @@ import javax.servlet.http.HttpServletResponse;
  * @author guillermo
  */
 public class MonitoringServlet extends HttpServlet {
+
+    private static final Logger LOGGER = Logger.getLogger(MonitoringServlet.class.getName());
+    
+    @Override
+    public void init() throws ServletException {
+        LOGGER.log(Level.INFO, "Initializing {0}", getServletConfig().getServletName());
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- examples/container-examplelibrary/src/main/java/module-info.java
+++ examples/container-examplelibrary/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module es.guillermogonzalezdeaguero.container.ExampleLibrary {
+    requires java.logging;
     requires java.servlet;
     exports es.guillermogonzalezdeaguero.container.examplelibrary;
 }
