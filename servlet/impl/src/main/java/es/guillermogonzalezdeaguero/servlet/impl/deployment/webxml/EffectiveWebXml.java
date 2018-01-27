f9841527138fed64347466a755ed25f556e7bdca
Guillermo González de Agüero <z06.guillermo@gmail.com>
2018-01-27 15:42:08 +0100

    optimized Filter and Servlet detection

--- servlet/impl/src/main/java/es/guillermogonzalezdeaguero/servlet/impl/deployment/webxml/EffectiveWebXml.java
+++ servlet/impl/src/main/java/es/guillermogonzalezdeaguero/servlet/impl/deployment/webxml/EffectiveWebXml.java
@@ -4,15 +4,17 @@ import es.guillermogonzalezdeaguero.servlet.impl.com.sun.java.xml.ns.javaee.Filt
 import es.guillermogonzalezdeaguero.servlet.impl.com.sun.java.xml.ns.javaee.FilterType;
 import es.guillermogonzalezdeaguero.servlet.impl.com.sun.java.xml.ns.javaee.ObjectFactory;
 import es.guillermogonzalezdeaguero.servlet.impl.com.sun.java.xml.ns.javaee.ServletMappingType;
-import es.guillermogonzalezdeaguero.servlet.impl.com.sun.java.xml.ns.javaee.ServletType;
 import es.guillermogonzalezdeaguero.servlet.impl.com.sun.java.xml.ns.javaee.WebApp;
 import es.guillermogonzalezdeaguero.servlet.impl.deployment.webxml.descriptor.FilterDescriptor;
 import es.guillermogonzalezdeaguero.servlet.impl.deployment.webxml.descriptor.ServletDescriptor;
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toSet;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
@@ -47,35 +49,33 @@ public class EffectiveWebXml {
         }
     }
 
-    private static Set<ServletDescriptor> findServlets(WebApp webApp, ClassLoader classLoader) throws ClassNotFoundException {
-        Set<ServletDescriptor> servletDescriptors = new HashSet<>();
-        for (ServletType servlet : webApp.getServlets()) {
-            List<ServletMappingType> mappings = new ArrayList<>();
-            for (ServletMappingType servletMapping : webApp.getServletMappings()) {
-                if (servletMapping.getServletName().getValue().equals(servlet.getServletName().getValue())) {
-                    mappings.add(servletMapping);
-                }
-            }
-            
-            servletDescriptors.add(new ServletDescriptor(servlet, mappings, classLoader));
-        }
+    private static Set<ServletDescriptor> findServlets(WebApp webApp, ClassLoader classLoader) {
+        Map<String, List<ServletMappingType>> collect = webApp.getServletMappings().
+                stream().
+                collect(Collectors.groupingBy(smt -> smt.getServletName().getValue()));
 
-        return servletDescriptors;
+        return webApp.getServlets().
+                stream().
+                map(st -> {
+                    try {
+                        return new ServletDescriptor(st, collect.getOrDefault(st.getServletName().getValue(), Collections.emptyList()), classLoader);
+                    } catch (ClassNotFoundException ex) {
+                        throw new WebXmlProcessingException(ex);
+                    }
+                }).collect(toSet());
     }
 
     private static Set<FilterDescriptor> findFilters(WebApp webApp, ClassLoader classLoader) throws ClassNotFoundException {
+        Map<String, List<FilterMappingType>> collect = webApp.getFilterMappings().
+                stream().
+                collect(Collectors.groupingBy(s -> s.getFilterName().getValue()));
+
         Set<FilterDescriptor> filterDescriptors = new HashSet<>();
-        
+
         // TODO: Check whether position counts from definition or mapping of filters
         int position = 0;
         for (FilterType filter : webApp.getFilters()) {
-            List<FilterMappingType> mappings = new ArrayList<>();
-            for (FilterMappingType filterMapping : webApp.getFilterMappings()) {
-                if (filterMapping.getFilterName().getValue().equals(filter.getFilterName().getValue())) {
-                    mappings.add(filterMapping);
-                }
-            }
-            filterDescriptors.add(new FilterDescriptor(filter, mappings, classLoader, ++position));
+            filterDescriptors.add(new FilterDescriptor(filter, collect.getOrDefault(filter.getFilterName().getValue(), Collections.emptyList()), classLoader, ++position));
         }
 
         return filterDescriptors;
