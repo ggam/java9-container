6c5046b22d2d8afb7c9d150d63cdf5d9aff2a068
Guillermo González de Agüero <z06.guillermo@gmail.com>
2018-03-02 19:06:47 +0100

    Removed unneeded Servlet dependency

--- impl/src/main/java/module-info.java
+++ impl/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module eu.ggam.container.Impl {
     requires java.logging;
-    requires java.servlet;
     requires eu.ggam.container.Api;
     
     uses eu.ggam.container.api.event.ServerLifeCycleListener;
